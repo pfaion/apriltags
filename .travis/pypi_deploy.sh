6e1903edae8e6d5ff658a9b578dd5b1ff5db9c5c
Patrick Faion <pfa@pupil-labs.com>
2019-08-23 16:04:27 +0200

    Debug windows dll built on travis

--- .travis/pypi_deploy.sh
+++ .travis/pypi_deploy.sh
@@ -3,6 +3,7 @@
 pip install twine || pip3 install twine
 python setup.py sdist || python3 setup.py sdist
 twine upload \
+    --repository-url https://test.pypi.org/legacy/
     --username "pupil-labs" \
     --password "$PyPIPassword" \
     --skip-existing \
